fdb59e21fed6c1524d6bc64f1c99a0b1a5bf3ed8
Ashutosh Gautam <ashutoshgautam9628@outlook.com>
2023-01-07 02:18:26 +0530

    Add files via upload

---
+++ build/precache-manifest.18c1b0ee91dc4f9199a6058bfc52e5f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "838b66f9a9a8130122cb8934d864a696",
+    "url": "/index.html"
+  },
+  {
+    "revision": "866ec0b99d426387e1de",
+    "url": "/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "fddcae3adb23c437c70b",
+    "url": "/static/css/main.972fbc83.chunk.css"
+  },
+  {
+    "revision": "866ec0b99d426387e1de",
+    "url": "/static/js/2.3c5c54fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3c5c54fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fddcae3adb23c437c70b",
+    "url": "/static/js/main.759d1ff1.chunk.js"
+  },
+  {
+    "revision": "cb5c3f098ed7fc2988d2",
+    "url": "/static/js/runtime-main.d6b47f06.js"
+  }
+]);
